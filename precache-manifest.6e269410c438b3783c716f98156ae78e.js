350da2310a997e51416cf6d09c30323b1bb6a52b
BROWN <klasik.corp30@gmail.com>
2019-08-16 15:53:21 -0500

    initial commit

---
+++ precache-manifest.6e269410c438b3783c716f98156ae78e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03869b680c439b96b7f03250a8ca083a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99547ae781fb671e0c6c",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fd7be82e3b062431a5ff",
+    "url": "/static/css/main.9ca39197.chunk.css"
+  },
+  {
+    "revision": "99547ae781fb671e0c6c",
+    "url": "/static/js/2.54cb0b07.chunk.js"
+  },
+  {
+    "revision": "fd7be82e3b062431a5ff",
+    "url": "/static/js/main.4b31fc60.chunk.js"
+  },
+  {
+    "revision": "4a0ae127cb0ec00f985f",
+    "url": "/static/js/runtime~main.6648f45d.js"
+  }
+]);
